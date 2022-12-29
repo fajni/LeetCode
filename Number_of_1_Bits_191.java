ce67a8cd476454d3dc883e37f13e4ec0dd43b638
small_duck <keychiper.cy@gmail.com>
2022-12-29 17:53:57 +0100

    Added 191. Number of 1 Bits

---
+++ Number_of_1_Bits_191.java
@@ -0,0 +1,28 @@
+package Problems;
+
+import java.util.*;
+
+public class Number_of_1_Bits_191 {
+
+    public static int hammingWeight(int n) {
+        int counter = 0, i =0;
+        String s;
+        s = Integer.toBinaryString(n);
+        System.out.println("S: " + s);
+
+        while(i<s.length()){
+            if(s.charAt(i)=='1')
+                counter++;
+            i++;
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 00000000000000000000000010000000;
+        System.out.println(hammingWeight(n));
+        //System.out.println(n);
+    }
+}
